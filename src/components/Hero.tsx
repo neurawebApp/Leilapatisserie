861e0784f531e5159b2dea64b39f9f91b984901a
neurawebApp <contact@neuraweb.tech>
2026-01-09 10:11:35 -0500

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
   };
 
   return (
-    <section id="home" className="relative h-screen flex items-center justify-center overflow-hidden">
+    <section id="home" className="relative h-screen w-full flex items-center justify-center overflow-hidden">
       <div
         className="absolute inset-0 z-0"
         style={{
